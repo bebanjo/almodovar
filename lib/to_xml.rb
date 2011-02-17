7a0de521edc9091c0cff77ef5071a315860d69a6
Sergio Gil Pérez de la Manga <sgilperez@gmail.com>
2011-02-17 12:56:04 +0100

    Refactor ToXml

--- lib/to_xml.rb
+++ lib/to_xml.rb
@@ -1,19 +1,11 @@
 require 'active_support/all'
 
 module Almodovar
-  
-  module ToXml
-    
-    def to_xml_with_links(options = {})
-      if options[:convert_links] && options[:builder]
-        options[:builder].tag!(:link, :rel => options[:root]) do
-          to_xml_without_links(options)
-        end
-      else
-        to_xml_without_links(options)
-      end
+  module ArrayToXml
+    def to_xml(options = {})
+      return super unless options[:convert_links]
+      options[:builder].tag!(:link, :rel => options[:root]) { super }
     end
-    
   end
   
   class Resource
@@ -21,10 +13,8 @@ module Almodovar
       options[:builder].tag!(:link, :rel => options[:root], :href => url)
     end
   end
-  
 end
 
 class Array
-  include Almodovar::ToXml
-  alias_method_chain :to_xml, :links
+  include Almodovar::ArrayToXml
 end
