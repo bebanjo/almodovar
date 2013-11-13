9882a8851e2eb0fb2fbb9caaad436ab587bcdc79
Luismi Cavalle <luismi@lmcavalle.com>
2013-11-13 10:52:47 +0000

    Show prev link before next link

--- lib/almodovar/resource_presenter/collection.rb
+++ lib/almodovar/resource_presenter/collection.rb
@@ -28,8 +28,8 @@ module Almodovar
         xml.instruct! unless options[:skip_instruct]
         xml.tag! resource_type.pluralize.dasherize, :type => 'array' do
           xml.tag!('total-entries', @total) if @total
-          next_link.to_xml(:builder => xml) if next_link
           prev_link.to_xml(:builder => xml) if prev_link
+          next_link.to_xml(:builder => xml) if next_link
           @resources.each { |value| value.to_xml(options.merge(:root => resource_type.singularize, :skip_instruct => true)) }
         end
       end
@@ -37,8 +37,8 @@ module Almodovar
       def as_json(options = {})
         message = ActiveSupport::OrderedHash.new.tap do |message|
           message[:total_entries] = @total if @total
-          message.merge! next_link.as_json if next_link 
           message.merge! prev_link.as_json if prev_link
+          message.merge! next_link.as_json if next_link 
           message[:entries] = @resources.map { |resource| resource.as_json(options) }
         end
       end
