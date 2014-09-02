44d6676498c305d80fd3d0f230813e8401750026
Amaia Castro <amaia@amaiac.net>
2014-09-02 15:49:00 +0200

    Store response body and status in Almodovar::HttpError
    This is useful when there is an error in a call to the Movida API, for example,
    and you want to be able to access the actual error message in the response 
    body, to know why the call failed. 
    For example, in the Movida API if you try to create a record and some 
    validation fails the response xml will contain the validation error message. 
    Having access to this makes debugging much easier.

--- lib/almodovar/errors.rb
+++ lib/almodovar/errors.rb
@@ -1,6 +1,10 @@
 module Almodovar
   class HttpError < Exception
+    attr_reader :response_status, :response_body
+
     def initialize(response, url)
+      @response_status = response.status
+      @response_body = response.body
       super("Status code #{response.status} on resource #{url}")
     end
   end
