a701c1b626f8c1b26dbafa4730321a1a8402efaa
Paco Guzman <pacoguzmanp@gmail.com>
2016-08-30 13:23:35 +0200

    On 422 response code raise a UnprocessableEntityError instance

--- lib/almodovar/errors.rb
+++ lib/almodovar/errors.rb
@@ -2,6 +2,7 @@ module Almodovar
   class HttpError < StandardError
     attr_reader :response_status, :response_body
 
+    # Children of this class must not override the initialize method
     def initialize(response, url)
       @response_status = response.status
       @response_body = response.body
@@ -20,4 +21,46 @@ module Almodovar
 
   class ConnectTimeoutError < TimeoutError
   end
+
+  class UnprocessableEntityError < HttpError
+    def errors?
+      error_messages.any?
+    end
+
+    # There are some different kind of errors in Almodovar servers
+    # "errors" => [{"error" => "wadus"}]
+    # "errors" => {"error" => ["wadus", "foo"]
+    # "error" => {"message" => "chaflan"}
+    #
+    # In case we cannot parse the response an empty array is returned
+    def error_messages
+      @error_messages ||= begin
+        if hash.key?('errors')
+          Array.wrap(hash['errors']).flat_map do |error|
+            error.is_a?(Hash) && error.key?('error') ? error['error'] : error.to_s
+          end
+        elsif hash.key?('error')
+          Array.wrap(hash['error'] && hash['error']['message'])
+        else
+          []
+        end
+      end
+    end
+
+    private
+
+    def hash
+      @hash ||= begin
+        Hash.from_xml(@response_body) || {}
+      rescue StandardError
+        # The expected errors depends on the ActiveSupport::XmlMini_parser used that it's REXML by default but there
+        # are other, so just rescue a generic error
+        {}
+      end
+    end
+  end
+
+  HTTP_ERRORS = {
+    422 => UnprocessableEntityError
+  }
 end
---
+++ spec/unit/unprocessable_entity_error_spec.rb
@@ -0,0 +1,70 @@
+require 'spec_helper'
+
+describe Almodovar::UnprocessableEntityError do
+  describe '#errors?' do
+    it 'returns true if there is any error message' do
+      error = unprocessable_entity_error(double_response(''))
+      expect(error).to receive(:error_messages).and_return(['wadus'])
+
+      expect(error.errors?).to eq true
+    end
+
+    it 'returns false if there is no error messages' do
+      error = unprocessable_entity_error(double_response(''))
+      expect(error).to receive(:error_messages).and_return([])
+
+      expect(error.errors?).to eq false
+    end
+  end
+
+  describe '#error_messages' do
+    it 'returns error messages for different schemes' do
+      body = %q{
+        <errors>
+          <error>Name is taken</error>
+        </errors>
+      }
+      error = unprocessable_entity_error(double_response(body))
+      error.error_messages.should eq(['Name is taken'])
+
+
+      body = %q{
+        <errors>
+          <error>Name is taken</error>
+          <error>External ID is required</error>
+        </errors>
+      }
+      error = unprocessable_entity_error(double_response(body))
+      error.error_messages.should eq(['Name is taken', 'External ID is required'])
+
+      body = %q{
+        <error>
+          <message>per_page maximum value is 200</message>
+        </error>
+      }
+      error = unprocessable_entity_error(double_response(body))
+      error.error_messages.should eq(['per_page maximum value is 200'])
+    end
+
+    it 'returns an empty array on empty response bodies' do
+      error = unprocessable_entity_error(double_response(''))
+      error.error_messages.should eq([])
+
+      error = unprocessable_entity_error(double_response(nil))
+      error.error_messages.should eq([])
+    end
+
+    it 'returns an empty array on json response bodies (it\'s not supported from the moment)' do
+      error = unprocessable_entity_error(double_response('{"wadus": "foo"}'))
+      error.error_messages.should eq([])
+    end
+  end
+
+  def double_response(body)
+    double(:response, body: body, status: nil)
+  end
+
+  def unprocessable_entity_error(response)
+    Almodovar::UnprocessableEntityError.new(response, 'http://example.com/api')
+  end
+end
