f37c74f5f07124bfe473d355375b4ab2725d5441
David Arango <arango@gmail.com>
2019-08-21 08:33:30 +0200

    Remove unused RSpec matchers

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,5 @@
 require 'rubygems'
 require 'webmock/rspec'
-require 'lorax'
 require 'almodovar'
 
 module Helpers
@@ -36,64 +35,5 @@ RSpec.configure do |config|
   config.after(:each) do
     WebMock.reset!
     Almodovar.default_options = $default_options
-  end
-end
-
-module NokogiriMatchers
-  RSpec::Matchers.define :have_processing_instruction do |processing_instruction|
-    match do |xml_string|
-      xml_string.starts_with?(processing_instruction)
-    end
-
-    failure_message do |xml_string|
-      "expected to have #{processing_instruction} at the beggining of xml:\n#{xml_string}"
-    end
-  end
-
-  RSpec::Matchers.define :match_xpath do |xpath|
-    match do |xml_string|
-      Nokogiri::XML.parse(xml_string).at_xpath(xpath) != nil
-    end
-
-    failure_message do |xml_string|
-      "expected to match xpath #{xpath} in xml:\n#{xml_string}"
-    end
-
-    failure_message_when_negated do |xml_string|
-      "expected to not match xpath #{xpath} in xml:\n#{xml_string}"
-    end
-
-  end
-
-  RSpec::Matchers.define :equal_xml do |expected_xml_string|
-    match do |actual_xml_string|
-      @expected_doc = Nokogiri.parse(expected_xml_string)
-      @actual_doc   = Nokogiri.parse(actual_xml_string)
-      Lorax::Signature.new(@expected_doc.root).signature == Lorax::Signature.new(@actual_doc.root).signature
-    end
-
-    failure_message do |actual_xml_string|
-      "XML documents expected to be the same:\n\nExpected:\n#{expected_xml_string}\n\nActual:\n#{actual_xml_string}"
-    end
-
-    failure_message_when_negated do |actual_xml_string|
-      "XML documents expected to be different but are equal"
-    end
-
-  end
-
-  RSpec::Matchers.define :have_text do |expected_text|
-    match do |actual_xml_string|
-      actual_text = Nokogiri.parse(actual_xml_string).text
-      actual_text.gsub(/\s+/,' ').include? expected_text.gsub(/\s+/,' ')
-    end
-
-    failure_message do |actual_xml_string|
-      "Document expected to contain \"#{expected_text}\". Actual:\n#{actual_xml_string}"
-    end
-
-    failure_message_when_negated do |actual_xml_string|
-      "Document expected to not contain \"#{expected_text}\". Actual:\n#{actual_xml_string}"
-    end
   end
 end
