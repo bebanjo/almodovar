075bbaeb02e76f5776400b117f44b06424917536
Jose Caburrasi <jose.caburrasi@mediagenix.tv>
2026-01-30 10:43:12 +0100

    reverting modify disable_duration

--- lib/almodovar/disable_duration_parsing.rb
+++ lib/almodovar/disable_duration_parsing.rb
@@ -1,8 +1,8 @@
 # Disable automatic duration parsing in ActiveSupport::XmlMini for Rails 8+
 # This fix removes the 'duration' parser so non-ISO values don't crash
 
-module ActiveSupport
-  module XmlMini
-    PARSING = PARSING.except("duration").freeze
-  end
-end
+# module ActiveSupport
+#   module XmlMini
+#     PARSING = PARSING.except("duration").freeze
+#   end
+# end
